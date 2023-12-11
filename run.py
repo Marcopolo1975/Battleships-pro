50730e45b1725e66e7c658124c2b66a7adec1099
Rehan Ahmad Rai <rehan.ahmad.rai@gmail.com>
2023-12-11 21:48:51 +0000

    add battleship graphic function

--- run.py
+++ run.py
@@ -1,2 +1,27 @@
 # import Function
 import random
+
+
+   # Function for welcome message
+    
+def start():
+   
+    print('''
+          
+                             __/___\_
+              _____/______\_____
+       ______/_____   _____\______\_____
+   __/_____/______\_/_______\_______\____\__
+  /_______________________________________\_
+  \_______________________________________/
+BBBBB      A    TTTTTT  TTTTTT  L       EEEEE   SSSSS  HH   HH  III  PPPP
+B    B    A A     TT      TT    L       E      S       HH   HH   I   P   P
+BBBBB    AAAAA    TT      TT    L       EEEE    SSSS   HHHHHHH   I   PPPP
+B    B  A     A   TT      TT    L       E          S   HH   HH   I   P
+BBBBB   A     A   TT      TT    LLLLLL  EEEEE  SSSSS   HH   HH  III  P
+
+
+    ''')
+
+
+start()
